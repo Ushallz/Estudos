41da9aa834d2720f08b66a51596bdaabf1612d61
Ushallz <thiago9campos@gmail.com>
2025-11-02 12:52:20 -0300

    Renomeia pastas.

---
+++ Python/Curso_em_video/Mundo 1/Aula 4/Desafio 1.py
@@ -0,0 +1,2 @@
+nome=input('qual é o seu nome?')
+print('Seja bem vindo', nome, 'é um prazer te conhecer!.')
---
+++ Python/Curso_em_video/Mundo 1/Aula 4/Desafio 2.py
@@ -0,0 +1,4 @@
+nome=input('qual é o seu nome?')
+idade=input('qual é a sua idade?')
+peso=input('quantos kgs você pesa?')
+print('seu nome é', nome, 'você pesa',peso, 'kgs e tem', idade, 'anos de idade.')
