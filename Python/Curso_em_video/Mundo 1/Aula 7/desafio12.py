6769abc4495d1283c3e9120cc0cbd2b22b7d11ea
Ushallzy <thiago9campos@gmail.com>
2025-11-20 19:18:08 -0300

    Update desafio12.py

--- Python/Curso_em_video/Mundo 1/Aula 7/desafio12.py
+++ Python/Curso_em_video/Mundo 1/Aula 7/desafio12.py
@@ -1,3 +1,3 @@
 discount=float(input('Digite o valor do item a receber o desconto'))
 
-print('o valor de {} com o desconto aplicado resultará em {}'.format((discount),(discount-(discount*5/100))))
+print('o valor de {} com o desconto aplicado resultará em {}'.format((discount),(discount-(discount*5/100)))) 
