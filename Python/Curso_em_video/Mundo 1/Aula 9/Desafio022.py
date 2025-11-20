3cc928f674cf99e052327e906147dc2c69a27401
Ushallz <thiago9campos@gmail.com>
2025-11-20 19:15:18 -0300

    Resolve desafios do curso

---
+++ Python/Curso_em_video/Mundo 1/Aula 9/Desafio022.py
@@ -0,0 +1,12 @@
+Nome=input('digite o seu nome completo')
+
+print(f'O seu nome com letras minúsculas é {Nome.lower()}')
+
+print(f'O seu nome, com letras maiúsculas é {Nome.upper()}')
+Space=Nome.count(' ')
+
+print(f'Seu nome inteiro possui {len(Nome)-Space} letras')
+
+Firstname=Nome.split()
+
+print(f'seu primeiro nome possui {len(Firstname[0])}')
