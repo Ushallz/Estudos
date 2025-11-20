0f97dc467728a1d9f46503cc6cacbf660b0338a7
Ushallzy <thiago9campos@gmail.com>
2025-11-20 19:17:34 -0300

    Update Desafio022.py

--- Python/Curso_em_video/Mundo 1/Aula 9/Desafio022.py
+++ Python/Curso_em_video/Mundo 1/Aula 9/Desafio022.py
@@ -3,6 +3,7 @@ Nome=input('digite o seu nome completo')
 print(f'O seu nome com letras minúsculas é {Nome.lower()}')
 
 print(f'O seu nome, com letras maiúsculas é {Nome.upper()}')
+
 Space=Nome.count(' ')
 
 print(f'Seu nome inteiro possui {len(Nome)-Space} letras')
